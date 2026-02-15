a43105ecfe52103787a1de014487f0132323ea8b
rookie-tanaka <tonkatu0741@gmail.com>
2026-02-15 19:53:35 +0900

    値変更時に現時刻を保存するよう追加

--- app/src/main/java/jp/tanaka/fusen/FusenRepository.kt
+++ app/src/main/java/jp/tanaka/fusen/FusenRepository.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.longPreferencesKey
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import kotlinx.coroutines.flow.Flow
@@ -17,11 +18,15 @@ val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "fu
 class FusenRepository(private val dataStore: DataStore<Preferences>) {
     // 保存するキーを定義
     private val MEMO_KEY = stringPreferencesKey("memo_number")
+    private val TIMESTAMP_KEY = longPreferencesKey("memo_timestamp")
 
     // Flowで読み込み
     // DataStoreはFlowを返すため、リアルタイムに更新を監視できる
-    val memoFlow: Flow<String> = dataStore.data.map { preferences ->
-        preferences[MEMO_KEY] ?: ""
+    val memoFlow: Flow<FusenData> = dataStore.data
+        .map { preferences ->
+        val number = preferences[MEMO_KEY] ?: ""
+        val timestamp = preferences[TIMESTAMP_KEY] ?: 0L
+        FusenData(number, timestamp)
     }
 
     // Suspend functionで書き込み
@@ -29,6 +34,7 @@ class FusenRepository(private val dataStore: DataStore<Preferences>) {
     suspend fun saveMemo(number: String) {
         dataStore.edit { preferences ->
             preferences[MEMO_KEY] = number
+            preferences[TIMESTAMP_KEY] = System.currentTimeMillis()
         }
     }
 }
